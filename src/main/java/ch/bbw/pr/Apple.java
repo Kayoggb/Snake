5206127c9fe15d200dd50b4614525ead5bc1ab57
Kaya <kaya.yongaci@icloud.com>
2025-10-03 13:17:20 +0200

    position und apple

---
+++ src/main/java/ch/bbw/pr/Apple.java
@@ -0,0 +1,19 @@
+package ch.bbw.pr;
+
+import javax.swing.text.Position;
+
+public class Apple {
+    private Position position;
+
+    public Apple (Position position) {
+        this.position = position;
+    }
+
+    public void Spawn(){
+
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+}
